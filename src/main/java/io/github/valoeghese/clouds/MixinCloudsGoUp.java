d54a7af98ca60b62aafa39e5f4998ebc8bb2a885
valoeghese <valoeghesemods@gmail.com>
2020-06-25 17:52:00 +1200

    1.0.1 change mixin

--- src/main/java/io/github/valoeghese/clouds/MixinCloudsGoUp.java
+++ src/main/java/io/github/valoeghese/clouds/MixinCloudsGoUp.java
@@ -1,12 +1,18 @@
 package io.github.valoeghese.clouds;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.render.SkyProperties;
 
-@Mixin(SkyProperties.Overworld.class)
+@Mixin(SkyProperties.class)
 public class MixinCloudsGoUp {
-	public float getCloudsHeight() {
-		return 200;
+	@Inject(at = @At("RETURN"), method = "getCloudsHeight", cancellable = true)
+	private void getCloudsHeight(CallbackInfoReturnable<Float> info) {
+		if (info.getReturnValueF() == 128.0F) {
+			info.setReturnValue(200.0F);
+		}
 	}
 }
